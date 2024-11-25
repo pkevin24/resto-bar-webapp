433b699726e67d959eb8364ca2cb759d39df1c36
Kevin Prince <kevinprince@Kevins-Laptop.local>
2024-11-25 12:57:10 +0530

    Initial commit

---
+++ src/test/java/com/rstobar/rest_bar_webapp/RestBarWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rstobar.rest_bar_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestBarWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
