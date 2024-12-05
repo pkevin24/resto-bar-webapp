50aaf79705c87ae036eb632dcc5db57a950eeb03
Kevin Prince <kevinprince@Kevins-Laptop.local>
2024-12-05 17:06:55 +0530

    Handler code updated and stack updation

--- cdk/lib/rest-bar-webapp-stack.ts
+++ cdk/lib/rest-bar-webapp-stack.ts
@@ -1,7 +1,11 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
-import * as lambda from 'aws-cdk-lib/aws-lambda'
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
+import * as rds from 'aws-cdk-lib/aws-rds';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class RestBarWebappStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -16,12 +20,81 @@ export class RestBarWebappStack extends cdk.Stack {
       }
     });
 
+    const vpc = new ec2.Vpc(this,'vpc',{
+      maxAzs:2,
+      subnetConfiguration: [
+        {
+          subnetType:ec2.SubnetType.PRIVATE_WITH_EGRESS,
+          name:"Private",
+        },
+        {
+          subnetType:ec2.SubnetType.PUBLIC,
+          name:"Public",
+        }
+      ]
+    }) 
+
+    const secret = new secretsmanager.Secret(this,"secret",{
+      secretName: "rdsCredentials",
+      generateSecretString:{
+        secretStringTemplate: JSON.stringify({ username: 'postgres' }),
+        generateStringKey: 'password'
+      }
+    });
+
+    const rdsSecuritygroup = new ec2.SecurityGroup(this,"rdsSecurtiyGroup",{
+      vpc,
+    })
+
+    const rdsInstance= new rds.DatabaseInstance(this, "RdsInstance", {
+      engine: rds.DatabaseInstanceEngine.postgres({version:rds.PostgresEngineVersion.VER_12_18}),
+      // credentials: {
+      //   username: secret.secretValueFromJson('username').toString(),
+      //   password: secret.secretValueFromJson('password')
+      // },
+      credentials:rds.Credentials.fromSecret(secret),
+      vpc,
+      vpcSubnets:{
+        subnetType:ec2.SubnetType.PRIVATE_WITH_EGRESS
+      },
+      securityGroups:[rdsSecuritygroup],
+      publiclyAccessible:false
+    });
+
+    const lambdaSecurityGroup = new ec2.SecurityGroup(this,"LambdaSecurityGroup",{
+      vpc
+    })
+
     const lambdaFunction = new lambda.Function(this,"lambdaFunction",{
       runtime: lambda.Runtime.JAVA_17,
       handler:'com.rstobar.rest_bar_webapp.Handler::handleRequest',
       code: lambda.Code.fromAsset('../target/rest-bar-webapp-0.0.1-SNAPSHOT.jar'),
       functionName:'LambdaFunctionForRestoBar',
+      vpc,
+      securityGroups:[lambdaSecurityGroup],
+      environment:{
+        SECRET_NAME : secret.secretName,
+        VPC_ID: vpc.vpcId,
+      },
+      timeout: cdk.Duration.seconds(20)
     })
+
+    //Granting lambda access to read permission for the secret
+    secret.grantRead(lambdaFunction);
+
+    //Allowing lambda to connect with RDS
+    rdsSecuritygroup.addIngressRule(lambdaSecurityGroup,ec2.Port.tcp(5432),'Allow Lambda to access to RDS');
+
+    // Add IAM permissions for Lambda to manage security groups
+    lambdaFunction.addToRolePolicy(new iam.PolicyStatement({
+      actions: [
+        'ec2:DescribeSecurityGroups',
+        'ec2:RevokeSecurityGroupIngress',
+        'ec2:RevokeSecurityGroupEgress',
+      ],
+      resources: ['*'], // Restrict further if needed
+    }));
+
 
     const endpoint1 = api.root.addResource('healthstatus');
     endpoint1.addMethod('GET',new apigateway.LambdaIntegration(lambdaFunction));
--- src/main/java/com/rstobar/rest_bar_webapp/Handler.java
+++ src/main/java/com/rstobar/rest_bar_webapp/Handler.java
@@ -4,11 +4,33 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;	
 
 public class Handler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent>{
 
+	
+	private static String secret;
+	static {
+        try {
+            String secretName = "rdsCredentials";
+            Region region = Region.of("us-east-1");
+            SecretsManagerClient client = SecretsManagerClient.builder().region(region).build();
+            GetSecretValueRequest request = GetSecretValueRequest.builder().secretId(secretName).build();
+            GetSecretValueResponse response = client.getSecretValue(request);
+            secret = response.secretString(); // Cache the secret
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+	
 	@Override
 	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent event, Context context) {
+		
+	    context.getLogger().log("Secret test:"+secret);
+	    
 		String path = event.getPath();
 		context.getLogger().log("Recieved request for the path:"+path);
 		APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
